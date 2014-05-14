bcba06e9cfe7f7e26289e71bd465dcb4c8782d30
lzc <495960481@qq.com>
2014-05-14 11:53:50 +0800

    提交

---
+++ testlightplugin/platforms/android/gen/cn/edu/gdmec/a07120726/testlightplugin/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.edu.gdmec.a07120726.testlightplugin;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
